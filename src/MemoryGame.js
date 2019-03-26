d1543edce5b7e1b35932fb0a664d64fc3e77f4b2
mohankumar <mohankumar73@hotmail.com>
2019-03-26 12:17:42 +0000

    final commit

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import card from './card';
+import Card from './Card';
 import shuffle from 'shuffle-array';
-import navbar from './navbar';
+import Navbar from './Navbar';
 
 const CardState = {
     HIDING: 0,
@@ -35,15 +35,78 @@ export default class MemoryGame extends Component {
     ];
     cards = shuffle(cards);
     this.state = {cards, noClick: false};
-  }  
+    
+    this.handleClick = this.handleClick.bind(this);
+    this.handleNewGame = this.handleNewGame.bind(this);
+  } 
+  
+  handleNewGame() {
+    let cards = this.state.cards.map(c => ({
+      ...c,
+      cardState: CardState.HIDING
+    }));
+    cards = shuffle(cards);
+    this.setState({cards});
+  }
+  
+  handleClick(id) {
+   const mapCardState = (cards, idsToChange, newCardState) => {
+     return cards.map(c => {
+       if(idsToChange.includes(c.id)) {
+         return {
+           ...c,
+           cardState: newCardState
+         };
+       }
+       return c;
+     });
+   };
+   
+   const foundCard = this.state.cards.find(c => c.id === id);
+   
+   if(this.state.noClick || foundCard.cardState !== CardState.HIDING) {
+     return;
+   }
+   
+   let noClick = false;
+   
+   let cards = mapCardState(this.state.cards, [id], CardState.SHOWING);
+   
+   const showingCards = cards.filter((c) => c.cardState === CardState.SHOWING);
+   
+   const ids = showingCards.map(c => c.id);
+   
+   if(showingCards.length === 2 && 
+      showingCards[0].backgroundColor === showingCards[1].backgroundColor) {
+        cards = mapCardState(cards, ids, CardState.MATCHING);
+      } else if (showingCards.length === 2) { 
+        let hidingCards = mapCardState(cards, ids, CardState.HIDING);
+        
+        noClick = true;
+        this.setState({cards, noClick}, () => {
+          setTimeout(() => {
+            this.setState({cards: hidingCards, noClick: false});
+          }, 1000);
+        });
+        return;
+      }
+      
+      this.setState({cards, noClick});
+  }
+  
   
   render() {
     const cards = this.state.cards.map((card) => (
-       <card key={card.id} />
+       <Card 
+          key={card.id} 
+          showing={card.cardState !== CardState.HIDING} 
+          backgroundColor={card.backgroundColor}
+          onClick={() => this.handleClick(card.id)}
+          />
       ));
     return (
       <div>
-        <navbar />
+        <Navbar onNewGame={this.handleNewGame}/>
         {cards}
       </div>
     );
